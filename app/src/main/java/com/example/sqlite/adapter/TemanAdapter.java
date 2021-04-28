a1d8e9f1d93dc649ebe7939c0849867383fac40e
ffadelm <faysalfadelm0107@gmail.com>
2021-04-28 22:01:36 +0700

    Actuvuty6

---
+++ app/src/main/java/com/example/sqlite/adapter/TemanAdapter.java
@@ -0,0 +1,63 @@
+package com.example.sqlite.adapter;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sqlite.R;
+import com.example.sqlite.Teman;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+
+    private ArrayList<Teman> listData;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    @Override
+    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman, parent, false);
+        return new TemanViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
+        String nm, tlp;
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+
+        holder.namaTxt.setTextSize(20);
+        holder.namaTxt.setText(nm);
+        holder.telponTxt.setText(tlp);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)?listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardKu;
+        private TextView namaTxt, telponTxt;
+        public TemanViewHolder(View view) {
+            super(view);
+            cardKu = view.findViewById(R.id.kartuKu);
+            namaTxt = view.findViewById(R.id.textNama);
+            telponTxt = view.findViewById(R.id.textTelpon);
+        }
+    }
+}
